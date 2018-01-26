e3870434ca17960eee15984e9a33555a99c6d8bd
JamesKimari <jamesgkimari@gmail.com>
2018-01-26 12:34:16 +0300

    defined the news class in news.py

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class News:
+    """
+    News class to define News objects
+    """
+
+    def __init__(self, id, author, title, description, url, urlToImage, publishedAt):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        
