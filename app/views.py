182afd98da474454b0ab51381f1bdecaa5e8d5b2
JamesKimari <jamesgkimari@gmail.com>
2018-01-26 14:11:34 +0300

    defined a function for getting the various news categories and also a process function to determine how results will appear

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ def index():
     """
     view root page function that returns the index page and its data
     """
+    # 
     title = "News Hub"
     return render_template('index.html', title=title)
 
--- request.py
+++ request.py
@@ -9,3 +9,42 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    """
+    Function that gets the json response to our url request
+    """
+    get_news_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+    
+    return news_results
+
+def process_results(news_list):
+    """
+    Function that processes the news result and transforms them to a list of objects
+    """
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        author = news_item.get('author')
+        title = news_item.get('title')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        urlToImage = news_item.get('urlToImage')
+        publishedAt = news_item.get('publishedAt')
+
+        if urlToImage:
+            news_object = News(id,author,title,description,url,urlToImage,publishedAt)
+            news_results.append(news_object)
+
+    return news_results
+
