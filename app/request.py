816f75a7e1fc2c7c1e20fe629e408cb99443828a
JamesKimari <jamesgkimari@gmail.com>
2018-01-26 15:35:16 +0300

    Created a macro and used it in index.html to pass in the different news categories

--- app/request.py
+++ app/request.py
@@ -2,7 +2,7 @@ from app import app
 import urllib.request, json
 from .models import news
 
-news = news.News
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -20,19 +20,19 @@ def get_news(category):
         get_news_data = url.read()
         get_news_response = json.loads(get_news_data)
 
-        news_results = None
+        news_articles = None
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
+        if get_news_response['articles']:
+            news_articles_list = get_news_response['articles']
+            news_articles = process_articles(news_articles_list)
     
-    return news_results
+    return news_articles
 
-def process_results(news_list):
+def process_articles(news_list):
     """
     Function that processes the news result and transforms them to a list of objects
     """
-    news_results = []
+    news_articles = []
     for news_item in news_list:
         id = news_item.get('id')
         author = news_item.get('author')
@@ -44,7 +44,7 @@ def process_results(news_list):
 
         if urlToImage:
             news_object = News(id,author,title,description,url,urlToImage,publishedAt)
-            news_results.append(news_object)
+            news_articles.append(news_object)
 
-    return news_results
+    return news_articles
 
--- app/views.py
+++ app/views.py
@@ -1,14 +1,21 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
     """
     view root page function that returns the index page and its data
     """
-    # 
+    # Getting business news
+    general_news = get_news('general')
+    business_news = get_news('business')
+    entertainment_news = get_news('entertainment')
+    sports_news = get_news('sports')
+    print(business_news)
     title = "News Hub"
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, general = general_news,
+     business=business_news, entertainment=entertainment_news, sports = sports_news)
 
 #views
 @app.route('/news/<int:news_id>')
