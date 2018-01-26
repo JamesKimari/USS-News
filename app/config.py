17382ab20e270c5aa23d0161dce320728f15e3dc
JamesKimari <jamesgkimari@gmail.com>
2018-01-26 12:05:56 +0300

    stored the news URL inside app.config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    MOVIE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey='
 
 
 class ProdConfig(Config):
