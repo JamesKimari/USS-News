f03b779d7372eb538402ddb0da84bd043ae424b3
JamesKimari <jamesgkimari@gmail.com>
2018-01-26 10:57:45 +0300

    created a new route in views.py that returns the index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from app import app
 
+@app.route('/')
+def index():
+    """
+    view root page function that returns the index page and its data
+    """
+    title = "News Hub"
+    return render_template('index.html', title=title)
+
 #views
 @app.route('/news/<int:news_id>')
 def news(news_id):
