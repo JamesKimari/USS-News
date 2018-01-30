04f5875a93e0f261dbde691de57e0635b53050a2
JamesKimari <jamesgkimari@gmail.com>
2018-01-30 05:01:40 +0300

    created a dynamic link to display business news when the image representing business news at index.html is clicked

--- app/main/views.py
+++ app/main/views.py
@@ -36,3 +36,13 @@ def general(category):
     news_categories_articles = get_category(category)
 
     return render_template('general.html', general = news_categories_articles)
+
+
+@main.route('/categories/<category>')
+def business(category):
+    """
+    view root page function that returns the categories page and its data
+    """
+    news_categories_articles = get_category(category)
+
+    return render_template('business.html', business = news_categories_articles)
